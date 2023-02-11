b6077d7aa4c76df6fda4d72bb0efd9249dfcda7e
Matteo Pidone <matteo.pidone@ctmobi.it>
2023-02-11 13:03:50 +0100

    feat(login): form di registrazione

--- APL-Project-Client/Form1.cs
+++ APL-Project-Client/Form1.cs
@@ -6,5 +6,45 @@ namespace APL_Project_Client
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void maskedTextBox1_MaskInputRejected(object sender, MaskInputRejectedEventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
     }
 }

4868cf700afbad1c14738ed876b94c6ac780caaa
Matteo Pidone <matteo.pidone@ctmobi.it>
2023-02-13 01:05:36 +0100

    feat(homepage): inserita homepage

---
+++ APL-Project-Client/Home.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace APL_Project_Client
+{
+    public partial class Home : Form
+    {
+        public Home()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            bool response = true;
+            if (response)
+            {
+                Form1 loginForm = new Form1();
+                loginForm.Show();
+                this.Hide();
+            }
+        }
+    }
+}

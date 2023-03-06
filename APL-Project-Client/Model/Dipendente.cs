cd1cdf0c173329188b8b55980489914ecb5dc484
Matteo Pidone <pidonematteo@hotmail.it>
2023-03-06 18:56:11 +0100

    feat(model): aggiunti modelli dipendente e ferie con i relativi metodi (provvisori)

---
+++ APL-Project-Client/Model/Dipendente.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace APL_Project_Client.Model;
+public class Dipendente
+{ //Farlo come classe astratta, poi dividere in Utente e Collega
+    public string nome;
+    public string cognome;
+    public string email;
+    private List<Ferie> listFerie;
+    public Dipendente(string nome, string cognome, string email)
+	{
+        this.nome = nome;
+        this.cognome = cognome; 
+        this.email = email;
+
+	}
+
+    public List<DateTime> getGiorniFerie()
+    {
+        if( listFerie != null )
+        {
+            //Thread.Sleep(3000);
+            return new List<DateTime>();
+            //chiamata http che valorizza e ritorna
+        } else
+        {
+            //Thread.Sleep(3000);
+            /*foreach (Ferie ferie in listFerie)
+            {            
+            }*/
+            List<DateTime> d = new List<DateTime>();
+            d.Add(new DateTime(2023, 3, 11));
+            d.Add(new DateTime(2023, 3, 14));
+            d.Add(new DateTime(2023, 3, 16));
+            return d;
+        }
+    }
+
+    public bool isGiornoFerie(DateTime date)
+    {
+        if( listFerie != null)
+        {
+        return !this.listFerie.Any(holiday => holiday.date.Year == holiday.date.Year && holiday.date.Month == holiday.date.Month && holiday.date.Day == date.Day);
+        }
+        return false;
+    }
+
+    public List<Dipendente> getDipendentiPresenti(DateTime date)
+    {
+        return new List<Dipendente>();
+    } 
+
+}
---
+++ APL-Project-Client/Model/Ferie.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace APL_Project_Client.Model;
+public class Ferie
+{
+    public DateTime date;
+    private string motivation;
+    public Ferie(int day, int month, int year, string motivation)
+    {
+        date = new DateTime(year, month, day);  
+        this.motivation = motivation;
+
+    }
+}
+
